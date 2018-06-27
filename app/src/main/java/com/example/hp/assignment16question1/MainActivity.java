4b7dc6c65fb2001fba7b390484e62c4940b6c97d
Aashish <aashishsahi@gmail.com>
2018-06-27 12:51:42 +0530

    First Commit

--- app/src/main/java/com/example/hp/assignment16question1/MainActivity.java
+++ app/src/main/java/com/example/hp/assignment16question1/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.hp.assignment16question1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("ActivityState", "onCreate");
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        Toast t = Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT);
+        t.setGravity(Gravity.CENTER,0,0);
+        t.show();
     }
 
     @Override
